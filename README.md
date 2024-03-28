0ae23d84498519365738dbe79ba97c1c60a028d0
sarmiente <53012993+sarmiente@users.noreply.github.com>
2024-03-28 12:52:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-yxdxze
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sarmiente/vitejs-vite-yxdxze)
---
+++ main.js
@@ -0,0 +1,33 @@
+// URL de la API de BoredAPI
+const apiUrl = 'http://www.boredapi.com/api/activity/';
+
+// Función para obtener una actividad aleatoria
+async function getRandomActivity() {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(
+        `Error al obtener la actividad: ${response.status} - ${response.statusText}`
+      );
+    }
+    const data = await response.json();
+    return data.activity;
+  } catch (error) {
+    console.error('Error al procesar la solicitud:', error);
+    return null;
+  }
+}
+
+// Ejemplo de uso
+getRandomActivity()
+  .then((activity) => {
+    if (activity) {
+      console.log('Actividad aleatoria:', activity);
+      // Aquí puedes manipular la actividad aleatoria según tus necesidades
+    } else {
+      console.log('No se pudo obtener la actividad.');
+    }
+  })
+  .catch((error) => {
+    console.error('Error al obtener la actividad:', error);
+  });
